edccfbdb48a49857e83185dc9d88223a53b28b99
386083520 <386083520@qq.com>
2021-10-15 07:53:32 +0800

    模块化的进化过程-iife模式

---
+++ test1/3,IIFE模式-匿名函数自调用/module2.js
@@ -0,0 +1,9 @@
+(function () {
+    let gsd2 = 'gsd666'
+    function gsd21() {
+        console.log('gsd21:' + gsd2)
+    }
+    function gsd22() {
+        console.log('gsd22:' + gsd2)
+    }
+})()
