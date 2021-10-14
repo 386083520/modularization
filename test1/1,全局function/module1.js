e1e555473680e5905e9c668d89db82d04260cccd
386083520 <386083520@qq.com>
2021-10-15 07:34:15 +0800

    模块化的进化过程-全局function

---
+++ test1/1,全局function/module1.js
@@ -0,0 +1,7 @@
+let gsd1 = 'gsd666'
+function gsd11() {
+    console.log('gsd11:' + gsd1)
+}
+function gsd12() {
+    console.log('gsd12:' + gsd1)
+}
---
+++ test1/1,全局function/module2.js
@@ -0,0 +1,7 @@
+let gsd2 = 'gsd666'
+function gsd21() {
+    console.log('gsd21:' + gsd2)
+}
+function gsd22() {
+    console.log('gsd22:' + gsd2)
+}
