c6acd3cf0b9aa1e482545b9ef9b6b547b5ed1ec3
386083520 <386083520@qq.com>
2021-10-15 07:34:49 +0800

    模块化的进化过程-namespace模式

---
+++ test1/2,namespace模式/module1.js
@@ -0,0 +1,9 @@
+let module1 = {
+    gsd1: 'gsd666',
+    gsd11: function () {
+        console.log('gsd11:' + this.gsd1)
+    },
+    gsd12: function () {
+        console.log('gsd12:' + this.gsd1)
+    }
+}
---
+++ test1/2,namespace模式/module2.js
@@ -0,0 +1,9 @@
+let module2 = {
+    gsd2: 'gsd6668',
+    gsd11: function () {
+        console.log('gsd21:' + this.gsd2)
+    },
+    gsd22: function () {
+        console.log('gsd22:' + this.gsd2)
+    }
+}
